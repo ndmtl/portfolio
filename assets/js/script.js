5f6003bd99579f35ba3a7c183c2980f663522651
ndmtl <nathalie.depierre@gmail.com>
2026-09-03 10:17:09 -0400

    Dernières mises à jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,19 @@ menuToggle.addEventListener('click', function () {
 //Afffiche le copyright avec la date actuelle :)
 document.getElementById("year").textContent = new Date().getFullYear();
 
+//Fitre les projets dans la page projets
+filterCategory("all");
+
+function filterCategory(category) {
+    const items = document.getElementsByClassName("project__card");
+
+    for (let i = 0; i < items.length; i++) {
+
+        if (category === "all" || items[i].classList.contains(category)) {
+            items[i].classList.add("project__card--show");
+        } else {
+            items[i].classList.remove("project__card--show");
+        }
+    }
+
+}
